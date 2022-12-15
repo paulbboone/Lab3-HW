ad1361203ebba3ef6fc2150297a7538f49361a1d
NguyenLam <91767799+paulbboone@users.noreply.github.com>
2022-12-15 11:22:46 +0700

    Summary

---
+++ HW3/src/components/CV.js
@@ -0,0 +1,245 @@
+import React from "react";
+import "./CV.css";
+
+
+const CV = () => {
+  return (
+    <div id="container">
+        
+            <div className="top">
+              <div className="profile">
+                    <div className="color-profile">
+                        <center>
+                            <h1>Profile</h1>
+                        </center>
+                    </div>
+
+                    <dd></dd>
+                    <center>
+                        <p>I am a student.</p>
+                    </center>
+                    <hr></hr>
+                 </div>
+              
+
+               <div class="body-profile">
+                    <div class="about-me">
+                        <div class="color-about-me">
+                            <h2>About me</h2>
+                        </div>
+
+                        <p>I am a lovely cat. I have passion in IT and I want to become data analyst.</p>
+                    </div>
+                    <div class="image"></div>
+                    <div class="detail">
+
+                        <div class="color-details">
+                            <h2>Details </h2>
+                        </div>
+                        <p><b>Name:</b></p>
+                        <p>Nguyen Thi Chi Lam</p>
+                        <p><b>Age:</b></p>
+                        <p>21 years</p>
+                        <p><b>Location:</b></p>
+                        <p>UIT, VNU-HCM</p>
+                    </div>
+                 </div>
+             </div>
+
+             <div class="between">
+                <div class="Exp">
+                    <div class="color-exp"><br></br>
+                        <center>
+                            <h1>Experiences</h1>
+                        </center><br></br>
+                    </div>
+                    <hr></hr>
+                </div>
+                <br></br>
+                <div class="Edu">
+                    <div class="color-edu">
+                        <h2>Educations</h2>
+                    </div>
+                </div>
+                <br></br>
+                <div class="Edu-Inf">
+                    <div class="Education">
+
+                        <p><b>UIT,VNU-HCM</b> </p>
+                        Apr 2019 - June 2024
+                    </div>
+
+                    <div class="IS">
+                        <p><b>Information System</b> </p>
+                        The education was mainly System design-based course, but I also learned about Web, Data analysis and more.During my time in college, I specialized in OOP, data science. Now, I'm learning web development
+                    </div>
+
+
+                </div>
+
+                <div class="WorkExp">
+                    <hr></hr>
+                    <h2>Working Experiences</h2>
+
+                </div>
+
+
+                <div class="Work-Part">
+                    <div class="WE">
+
+                        <h3>Facebook I Fresher Engineer</h3>
+                        Sep 2021 - current
+                    </div>
+                    <div class="PF">
+                        <h3>Partime - Fresher Web Developer</h3>
+                        Working as a Fresher Developer on a function on Facebook page. Dating function is an app as Tinder
+                    </div>
+                </div>
+             </div>
+         
+
+      <div class="bottom">
+
+        <div class="Abilities">
+            <br></br>
+
+            <center>
+                <h1>Abilities</h1>
+            </center><br></br>
+        </div>
+        <hr></hr>
+        <div class="Skills">
+            <h2>Skills</h2>
+        </div>
+        <div class="Program-skills">
+
+            <div class="seven-skills1">
+                <p>HTML/CSS&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+
+                    <input type="checkbox" name="skill" value="HTML/CSS" checked></input>
+                    <input type="checkbox" name="skill" value="HTML/CSS" checked></input>
+                    <input type="checkbox" name="skill" value="HTML/CSS" checked></input>
+                    <input type="checkbox" name="skill" value="HTML/CSS" checked></input>
+                    <input type="checkbox" name="skill" value="HTML/CSS"></input>
+                </p>
+                <p>SQl&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                    <input type="checkbox" name="skill" value="SQL" checked></input>
+                    <input type="checkbox" name="skill" value="SQL"checked></input>
+                    <input type="checkbox" name="skill" value="SQL"checked></input>
+                    <input type="checkbox" name="skill" value="SQL"></input>
+                    <input type="checkbox" name="skill" value="SQL"></input>
+                </p>
+
+                <p>JQuery&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                    <input type="checkbox" name="skill" value="JQuery"checked></input>
+                    <input type="checkbox" name="skill" value="JQuery"checked></input>
+                    <input type="checkbox" name="skill" value="JQuery"checked></input>
+                    <input type="checkbox" name="skill" value="JQuery"></input>
+                    <input type="checkbox" name="skill" value="JQuery"></input>
+                </p>
+
+                <p>Javascript&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                    <input type="checkbox" name="skill" value="Javascript"checked></input>
+                    <input type="checkbox" name="skill" value="Javascript"checked></input>
+                    <input type="checkbox" name="skill" value="Javascript"checked></input>
+                    <input type="checkbox" name="skill" value="Javascript"></input>
+                    <input type="checkbox" name="skill" value="Javascript"></input>
+                </p>
+            </div>
+
+            <div class="seven-skills2">
+                <p>Object Orientated Programming &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                    <input type="checkbox" name="skill" value="Object Orientated Programming"checked></input>
+                    <input type="checkbox" name="skill" value="Object Orientated Programming"checked></input>
+                    <input type="checkbox" name="skill" value="Object Orientated Programming"checked></input>
+                    <input type="checkbox" name="skill" value="Object Orientated Programming"></input>
+                    <input type="checkbox" name="skill" value="Object Orientated Programming"></input>
+                </p>
+                <p>SEO&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                    <input type="checkbox" name="skill" value="SEO"checked></input>
+                    <input type="checkbox" name="skill" value="SEO"checked></input>
+                    <input type="checkbox" name="skill" value="SEO"checked></input>
+                    <input type="checkbox" name="skill" value="SEO"></input>
+                    <input type="checkbox" name="skill" value="SEO"></input>
+
+                </p>
+                <p>React&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                    <input type="checkbox" name="skill" value="React"checked></input>
+                    <input type="checkbox" name="skill" value="React"checked></input>
+                    <input type="checkbox" name="skill" value="React"checked></input>
+                    <input type="checkbox" name="skill" value="React"></input>
+                    <input type="checkbox" name="skill" value="React"></input>
+                </p>
+            </div>
+        </div>
+        <div class="github">
+            <div class="text">
+                <center>
+                    <i>See my profile on github</i>
+                </center>
+            </div>
+            <center>
+                <div class="button-git">
+                    <center>
+
+                        <a href="https://github.com/paulbboone">See project on Github
+                                </a>
+                    </center>
+                </div>
+            </center>
+        </div>
+
+
+        <hr></hr>
+        <div class="Languages">
+            <h2>Languages</h2>
+        </div>
+        <div class="four-languages1">
+            <p>
+                Vietnamese(Native)&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                <input type="checkbox" name="Languages" value="Vietnamese(Native)"checked></input>
+                <input type="checkbox" name="Languages" value="Vietnamese(Native)"checked></input>
+                <input type="checkbox" name="Languages" value="Vietnamese(Native)"checked></input>
+                <input type="checkbox" name="Languages" value="Vietnamese(Native)"checked></input>
+                <input type="checkbox" name="Languages" value="Vietnamese(Native)"checked></input>
+            </p>
+            <p>
+                English (IELTS 6.5)&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                <input type="checkbox" name="Languages" value="English (IELTS 6.5)"checked></input>
+                <input type="checkbox" name="Languages" value="English (IELTS 6.5)"checked></input>
+                <input type="checkbox" name="Languages" value="English (IELTS 6.5)"checked></input>
+                <input type="checkbox" name="Languages" value="English (IELTS 6.5)"></input>
+                <input type="checkbox" name="Languages" value="English (IELTS 6.5)"></input>
+            </p>
+        </div>
+        <div class="four-languages2">
+            <p>
+                Cat (Mother Tongue)&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                <input type="checkbox" name="Languages" value="Cat(Mother Tongue)"checked></input>
+                <input type="checkbox" name="Languages" value="Cat(Mother Tongue)"checked></input>
+                <input type="checkbox" name="Languages" value="Cat(Mother Tongue)"checked></input>
+                <input type="checkbox" name="Languages" value="Cat(Mother Tongue)"checked></input>
+                <input type="checkbox" name="Languages" value="Cat(Mother Tongue)"checked></input>
+            </p>
+            <p>
+                Dog (Foreign Language)&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp;&ensp; &ensp; &ensp; &ensp; &ensp; &ensp;
+                <input type="checkbox" name="Languages" value="Dog (Foreign Language)"checked></input>
+                <input type="checkbox" name="Languages" value="Dog (Foreign Language)"checked></input>
+                <input type="checkbox" name="Languages" value="Dog (Foreign Language)"></input>
+                <input type="checkbox" name="Languages" value="Dog (Foreign Language)"></input>
+                <input type="checkbox" name="Languages" value="Dog (Foreign Language)"></input>
+            </p>
+        </div>
+        </div>
+
+
+    </div>
+  
+    
+
+    
+
+  );
+};
+
+export default CV;
